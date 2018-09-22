766f352ce1553b4694b641ffebaee1d7faa4b5fc
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-09-22 16:31:28 +0100

    adding homepage query for items

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -3,10 +3,17 @@ const getAllItems = require("../model/queries/get_item");
 // handler for home page
 
 exports.get = (req, res) => {
-  res.render("home");
   getAllItems((err, result) => {
-    console.log("this is result in the server:", result);
+    console.log("this is allitems rows: ", result);
+    const items = result;
+    res.render("home", { items });
   });
+
+  // getAllCategories((err, result) => {
+  //   console.log(result);
+  //   const categories = result.rows;
+  //   res.render("home", { categories });
+  // })
 };
 
 // decide functionality (link to post/get requests and queries)
